708bc7f39d425a58f53265f9f313baa7494a588d
李一诺 <yinuo@eduroam-56-123.nomadic.bris.ac.uk>
2026-02-24 13:39:50 +0000

    new change

--- docs/js/PlayerCannon.js
+++ docs/js/PlayerCannon.js
@@ -2,10 +2,7 @@ class PlayerCannon {
     #positionVector;
     #wheelRadius;
     #barrelSize;
-<<<<<<< HEAD
-=======
     #barrelAngle = 0;
->>>>>>> 179dcb5bf8fe382958ada10e247a2614121db903
     #fillColor;
     #outlineColor;
 
@@ -17,8 +14,6 @@ class PlayerCannon {
         this.#outlineColor = outColor;
     }
 
-<<<<<<< HEAD
-=======
     fireShot(shotRadius) {
         // this value can be used for the power
         const SPEED = 300;
@@ -28,7 +23,6 @@ class PlayerCannon {
         offset.rotate(this.#barrelAngle);
         return new Projectile(p5.Vector.add(this.#positionVector, offset), velocity, shotRadius);
     }
->>>>>>> 179dcb5bf8fe382958ada10e247a2614121db903
     drawPlayer() {
         fill(this.#fillColor);
         stroke(this.#outlineColor);
@@ -36,29 +30,17 @@ class PlayerCannon {
         this.#drawWheel();
     }
 
-<<<<<<< HEAD
-    #drawWheel() {
-        circle(this.#positionVector.x, this.#positionVector.y,
-            this.#wheelRadius);
-=======
     set barrelAngle(needleAngle) { this.#barrelAngle = needleAngle; }
 
     #drawWheel() {
         circle(this.#positionVector.x, this.#positionVector.y, this.#wheelRadius);
->>>>>>> 179dcb5bf8fe382958ada10e247a2614121db903
     }
     #drawBarrel() {
         push();
         rectMode(CENTER);
         translate(this.#positionVector.x, this.#positionVector.y);
-<<<<<<< HEAD
-        rotate(controlPanel.angleDial.needleRotation - 90);
-        rect(this.#wheelRadius, 0,
-            this.#barrelSize.x, this.#barrelSize.y);
-=======
         rotate(this.#barrelAngle);
         rect(this.#wheelRadius, 0, this.#barrelSize.x, this.#barrelSize.y);
->>>>>>> 179dcb5bf8fe382958ada10e247a2614121db903
         pop();
     }
 }
