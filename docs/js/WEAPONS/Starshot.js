fd0fc458694f448ef34467582a98241dd05267f2
Yuxin <fx25208@bristol.ac.uk>
2026-04-21 22:54:38 +0100

    fixed bugs with weapon interface thumbnails

--- docs/js/WEAPONS/Starshot.js
+++ docs/js/WEAPONS/Starshot.js
@@ -18,7 +18,7 @@ class Starshot extends AbstractWeapon {
     if (typeof starImg !== "undefined" && starImg) {
       push();
       imageMode(CENTER);
-      const scale = Math.min((r * 2.8) / starImg.width, (r * 2.8) / starImg.height);
+      const scale = Math.min((r * 2.4) / starImg.width, (r * 2.4) / starImg.height);
       image(starImg, cx, cy, starImg.width * scale, starImg.height * scale);
       pop();
       return;
